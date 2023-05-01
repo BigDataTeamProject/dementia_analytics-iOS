caa0a1f91b208ae9873296e1827fce554cc85254
Jeonhui <dlwjsgml02@naver.com>
2023-05-01 12:16:43 +0900

    feat: base app settings

---
+++ dementia_analytics-iOS/dementia_analytics-iOS/Presentation/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  dementia_analytics-iOS
+//
+//  Created by 이전희 on 2023/05/01.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.view.backgroundColor = .green
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
